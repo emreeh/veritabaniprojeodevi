f2c91ba745a6a01439954144c93f9b9d4b2524bd
emreeh <72201253+emreeh@users.noreply.github.com>
2021-12-20 21:54:31 +0300

    csharp
    csharp kaynak kodu eklendi

---
+++ sql csharp kaynak kodları (visual studio)/Form1.cs
@@ -0,0 +1,77 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace postgresql_ödev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            baglanti.Open();
+            NpgsqlCommand komut1 = new NpgsqlCommand("insert into barista (baristaid,ad) values (@p1,@p2)", baglanti);
+            komut1.Parameters.AddWithValue("@p1", int.Parse(textBox1.Text ));
+            komut1.Parameters.AddWithValue("@p2", textBox2.Text );
+            komut1.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("ekleme işlemi başarılı bir şekilde gerçekleşti");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand komut3 = new NpgsqlCommand("update barista set baristaid=@p1,ad=@p2", baglanti);
+            komut3.Parameters.AddWithValue("@p1", int.Parse(textBox1.Text));
+            komut3.Parameters.AddWithValue("@p2", textBox2.Text);
+            MessageBox.Show("ürün güncelleme işlemi gerçekleşti", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            baglanti.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        NpgsqlConnection baglanti = new NpgsqlConnection("server=localHost; port=5432; Database=kahveticaretveritabaniodev; user ID=postgres; password=hasimgurdamar");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sorgu = "select * from barista";
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sorgu, baglanti);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand komut2 = new NpgsqlCommand("DElete from barista where baristaid=@p1", baglanti);
+            komut2.Parameters.AddWithValue("@p1", int.Parse(textBox1.Text));
+            komut2.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("eleman silme işlemi başarılı bir şekilde gerçekleşti", "bilgi", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+        }
+    }
+}
